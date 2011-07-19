c1a1780de21ccd9c50cb833eb19e6bad60dcab23
Colin Hebert <hebert.colin@gmail.com>
2011-07-19 20:36:23 +0100

    Emacs: Add line highlighting
    Highlight the current line

--- emacs.d/makkhdyn.el
+++ emacs.d/makkhdyn.el
@@ -1,6 +1,7 @@
 (personal 'global)
 (personal 'custom 'noerror)
 (personal 'bindings)
+(personal 'hl-line)
 
 (vendor 'color-theme)
 (vendor 'magit 'magit-status)
---
+++ emacs.d/makkhdyn/hl-line.el
@@ -0,0 +1,6 @@
+;;; Highlight current line
+
+(global-hl-line-mode 1)
+
+;; Great for black background
+(set-face-background 'hl-line "gray8")
